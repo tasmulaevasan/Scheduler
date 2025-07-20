778e2512c5c7bd56d00e0cb5e44266a9dd349ad0
Asan Tasmulaev <128286929+tasmulaevasan@users.noreply.github.com>
2025-07-20 21:02:51 +0500

    Add teachers page to sidebar

--- main_window.py
+++ main_window.py
@@ -1,7 +1,25 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QWidget
 from PyQt6 import uic
+
+
+class TeacherItem(QWidget):
+    def __init__(self, index=1, parent=None):
+        super().__init__(parent)
+        uic.loadUi("teachersItem.ui", self)
+        self.index_label.setText(str(index))
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main_window.ui", self)
+
+        self.teachers_page = uic.loadUi("teachers.ui")
+        self.stackedWidget.addWidget(self.teachers_page)
+        self.teachers_page.teachersListLayout.addWidget(TeacherItem())
+
+        self.teachers_btn_1.toggled.connect(self._show_teachers)
+        self.teachers_btn_2.toggled.connect(self._show_teachers)
+
+    def _show_teachers(self, checked):
+        if checked:
+            self.stackedWidget.setCurrentWidget(self.teachers_page)
